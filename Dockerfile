dc32dfc13b0cce28683fe626ff0f597fdcb1b988
Ruda Porto Filgueiras <ruda.filgueiras@olx.com>
2019-05-26 04:09:29 +0200

    Improve Dockerfile. Change app.py to use UJSONResponse. Disabled debug mode.

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.7
 COPY . /app
 WORKDIR /app
 
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip && pip install -r requirements.txt
 
 EXPOSE 8000
 
--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.responses import UJSONResponse
 import uvicorn
 
-app = Starlette(debug=True)
+app = Starlette(debug=False)
 
 
 @app.route('/')
 async def homepage(request):
-    return JSONResponse({'hello': 'world'})
+    return UJSONResponse({'hello': 'world'})
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
